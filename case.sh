9e5a286f8864fb3dad18c9f4be816ccbcf3e9394
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-03 20:31:06 +0530

    case

--- case.sh
+++ case.sh
@@ -1,1 +1,13 @@
-echo "hello"
+echo "Enter an alphabet"
+read charac
+case $charac in
+[aeiouAEIOU])
+echo "Vowel"
+;;
+[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ])
+echo "Consonant."
+;;
+*)
+echo "invalid output"
+esac
+
